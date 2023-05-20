079a8d98f38ae8f3d7647aa058fafa00323e24eb
howtodie123 <96249613+howtodie123@users.noreply.github.com>
2023-05-20 13:17:36 +0700

    Hoàn Thành Xây dựng Web Server API

---
+++ TestTestServer/TestTestServer/Controllers/CustomerController.cs
@@ -0,0 +1,88 @@
+﻿using TestTestServer.Data;
+using Microsoft.AspNetCore.Mvc;
+using TestTestServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTestServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+        private readonly APIData dbContext;
+
+        public CustomerController(APIData dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        // get: lấy dữ liệu
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await dbContext.Customer.ToListAsync());
+        }
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IActionResult> GetOne([FromRoute] int id)
+        {
+            var customer = await dbContext.Customer.FindAsync(id);
+            if (customer == null) { return NotFound(); }
+            return Ok(customer);
+        }
+        // post: tạo  mới
+        [HttpPost]
+        public async Task<IActionResult> Add(CustomerRequest addd)
+        {
+            var customer = new Customer()
+            {
+                // CusID = addd.CusID,
+                CusName = addd.CusName,
+                CusAddress = addd.CusAddress,
+                CusPhone = addd.CusPhone,
+                CusBirth = addd.CusBirth,
+                CusDateRegister = addd.CusDateRegister,
+                CusAccount = addd.CusAccount,
+                CusPassword = addd.CusPassword,
+            };
+            await dbContext.Customer.AddAsync(customer);
+            await dbContext.SaveChangesAsync();
+            return Ok(customer);
+        }
+        // put: chỉnh sửa dữ liệu
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, CustomerRequest Update)
+        {
+            var customer = await dbContext.Customer.FindAsync(id);
+            if (customer != null)
+            {
+                customer.CusName = Update.CusName;
+                customer.CusAddress = Update.CusAddress;
+                customer.CusPhone = Update.CusPhone;
+                customer.CusBirth = Update.CusBirth;
+                customer.CusDateRegister = Update.CusDateRegister;
+                customer.CusAccount = Update.CusAccount;
+                customer.CusPassword = Update.CusPassword;
+               
+                await dbContext.SaveChangesAsync();
+
+                return Ok(customer);
+            }
+            return NotFound();
+        }
+        // delete: xóa dữ liệu
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var customer = await dbContext.Customer.FindAsync(id);
+            if (customer != null)
+            {
+                dbContext.Remove(customer);
+                await dbContext.SaveChangesAsync();
+                return Ok(customer);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ TestTestServer/TestTestServer/Models/Ad.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTestServer.Models
+{
+    public class Ad
+    {
+        [Key]
+        public int AdID { get; set; }
+        public string? AdName { get; set; }
+        public string? AdAccount { get; set; }
+        public string? AdPassword { get; set; }
+    }
+}
